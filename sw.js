284cf16fc49e3505da871d9255112ee840ada014
santrodoha <trixiedeasis.tmjda@gmail.com>
2026-03-09 23:10:40 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'santro-doha-v1';
+const ASSETS = [
+  '/cleaning-company-website.html',
+  '/santro-admin-panel.html',
+  '/manifest.json',
+  '/manifest-admin.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (!res || res.status !== 200) return res;
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match('/cleaning-company-website.html'));
+    })
+  );
+});
